557df86eda10828e1d31b9e8cea93a53423794e7
Victor Martinez <victorm050402@gmail.com>
2023-04-07 19:07:01 -0300

    Proyecto 0.9

---
+++ app.js
@@ -0,0 +1,266 @@
+const stockProductos = [
+  {
+    id: 1,
+    nombre: "Angel Guardian",
+    cantidad: 1,
+    precio: 8500,
+    img: "./img/remeras/angel_guardian.png",
+  },
+  {
+    id: 2,
+    nombre: "Cosmos Blanca",
+    cantidad: 1,
+    precio: 7000,
+    img: "./img/remeras/cosmos_blanca.png",
+  },
+  {
+    id: 3,
+    nombre: "Cosmos Crema",
+    cantidad: 1,
+    precio: 7000,
+    img: "./img/remeras/cosmos_crema.png",
+  },
+  {
+    id: 4,
+    nombre: "Tokyo",
+    cantidad: 1,
+    precio: 7500,
+    img: "./img/remeras/tokyo.png",
+  },
+  {
+    id: 5,
+    nombre: "Hoddie Fuck Luv",
+    cantidad: 1,
+    precio: 15000,
+    img: "./img/hoodie_fuck.png",
+  },
+];
+
+//Declaraciones de variables
+
+let carrito = [];
+const contenedor = document.querySelector("#contenedor");
+const carritoContenedor = document.querySelector("#carritoContenedor");
+const vaciarCarrito = document.querySelector("#vaciarCarrito");
+const precioTotal = document.querySelector("#precioTotal");
+const activarFuncion = document.querySelector("#activarFuncion");
+const procesarCompra = document.querySelector("#procesarCompra");
+const totalProceso = document.querySelector("#totalProceso");
+const formulario = document.querySelector('#procesar-pago')
+
+//Funnciones del carrito 
+
+if (activarFuncion) {
+  activarFuncion.addEventListener("click", procesarPedido);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+  mostrarCarrito();
+  document.querySelector("#activarFuncion").click(procesarPedido);
+});
+if(formulario){
+  formulario.addEventListener('submit', enviarCompra)
+}
+
+
+if (vaciarCarrito) {
+  vaciarCarrito.addEventListener("click", () => {
+    carrito.length = [];
+    mostrarCarrito();
+  });
+}
+
+if (procesarCompra) {
+  procesarCompra.addEventListener("click", () => {
+    if (carrito.length === 0) {
+      Swal.fire({
+        title: "¡Tu carrito está vacio!",
+        text: "Compra algo para continuar con la compra",
+        icon: "error",
+        confirmButtonText: "Aceptar",
+      });
+    } else {
+      location.href = "compra.html";
+    }
+  });
+}
+
+//Productos
+
+stockProductos.forEach((prod) => {
+  const { id, nombre, precio, img } = prod;
+  if (contenedor) {
+    contenedor.innerHTML +=
+    `
+    <div>
+    <div class="porducto">
+        <img src="${img}" alt="${nombre}">
+        <div class="contenido_productos">
+            <p class="title">${nombre}</p>
+            <h4 class="precio">${precio}</h4>
+            <button class="btn_add" onclick="agregarProducto(${id})">Añadir al Carrito</button>
+        </div>
+    </div>
+</div>
+
+`;
+}
+});
+
+const agregarProducto = (id) => {
+  const existe = carrito.some(prod => prod.id === id)
+
+  if(existe){
+    const prod = carrito.map(prod => {
+      if(prod.id === id){
+        prod.cantidad++
+      }
+    })
+  } else {
+    const item = stockProductos.find((prod) => prod.id === id)
+    carrito.push(item)
+  }
+  mostrarCarrito()
+
+};
+
+//Carrito 
+
+const mostrarCarrito = () => {
+  const modalBody = document.querySelector(".modal .modal-body");
+  if (modalBody) {
+    modalBody.innerHTML = "";
+    carrito.forEach((prod) => {
+      const { id, nombre, precio, img, cantidad } = prod;
+      console.log(modalBody);
+      modalBody.innerHTML += `
+      <div class="modal-contenedor">
+        <div>
+        <img class="img-fluid img-carrito" src="${img}"/>
+        </div>
+        <div>
+        <p>Producto: ${nombre}</p>
+      <p>Precio: ${precio}</p>
+      <p>Cantidad :${cantidad}</p>
+      <button class="btn btn-danger"  onclick="eliminarProducto(${id})">Eliminar producto</button>
+        </div>
+      </div>
+      
+  
+      `;
+    });
+  }
+
+  if (carrito.length === 0) {
+    console.log("Nada");
+    modalBody.innerHTML = `
+    <p class="text-center text-primary parrafo">¡El carrito esta vacio!</p>
+    `;
+  } else {
+    console.log("Algo");
+  }
+  carritoContenedor.textContent = carrito.length;
+
+  if (precioTotal) {
+    precioTotal.innerText = carrito.reduce(
+      (acc, prod) => acc + prod.cantidad * prod.precio,
+      0
+    );
+  }
+
+  guardarStorage();
+};
+
+function guardarStorage() {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+function eliminarProducto(id) {
+  const remeraId = id;
+  carrito = carrito.filter((remera) => remera.id !== remeraId);
+  mostrarCarrito();
+}
+function procesarPedido() {
+  carrito.forEach((prod) => {
+    const listaCompra = document.querySelector("#lista-compra tbody");
+    const { id, nombre, precio, img, cantidad } = prod;
+    if (listaCompra) {
+      const row = document.createElement("tr");
+      row.innerHTML += `
+              <td>
+              <img class="img-fluid img-carrito" src="${img}"/>
+              </td>
+              <td>${nombre}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>${precio * cantidad}</td>
+            `;
+      listaCompra.appendChild(row);
+    }
+  });
+  totalProceso.innerText = carrito.reduce(
+    (acc, prod) => acc + prod.cantidad * prod.precio,
+    0
+  );
+}
+
+ function enviarCompra(e){
+   e.preventDefault()
+   const cliente = document.querySelector('#cliente').value
+   const email = document.querySelector('#correo').value
+
+   if(email === '' || cliente == ''){
+     Swal.fire({
+       title: "¡Debes completar tu email y nombre!",
+       text: "Rellena el formulario",
+       icon: "error",
+       confirmButtonText: "Aceptar",
+   })
+ } else {
+
+  const btn = document.getElementById('button');
+
+// document.getElementById('procesar-pago')
+//  .addEventListener('submit', function(event) {
+//    event.preventDefault();
+
+   btn.value = 'Enviando...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_qxwi0jn';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Finalizar compra';
+      alert('Correo enviado!');
+    }, (err) => {
+      btn.value = 'Finalizar compra';
+      alert(JSON.stringify(err));
+    });
+    
+   const spinner = document.querySelector('#spinner')
+   spinner.classList.add('d-flex')
+   spinner.classList.remove('d-none')
+
+   setTimeout(() => {
+     spinner.classList.remove('d-flex')
+     spinner.classList.add('d-none')
+     formulario.reset()
+
+     const alertExito = document.createElement('p')
+     alertExito.classList.add('alert', 'alerta', 'd-block', 'text-center', 'col-12', 'mt-2', 'alert-success')
+     alertExito.textContent = 'Compra realizada correctamente'
+     formulario.appendChild(alertExito)
+
+     setTimeout(() => {
+       alertExito.remove()
+     }, 3000)
+
+
+   }, 3000)
+ }
+ localStorage.clear()
+
+ }
